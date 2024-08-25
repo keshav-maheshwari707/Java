190f9e2d78e5a526f0cc0adefc57522baaac4e04
keshav-maheshwari707 <keshav.maheshwari707@gmail.com>
2024-08-25 23:50:05 +0530

    table

---
+++ Table.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String args[]) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter number :");
+        int number = myObj.nextInt();
+        int num1 = number * 1;
+        int num2 = number * 2;
+        int num3 = number * 3;
+        int num4 = number * 4;
+        int num5 = number * 5;
+        int num6 = number * 6;
+        int num7 = number * 7;
+        int num8 = number * 8;
+        int num9 = number * 9;
+        int num10 = number * 10;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(num7);
+        System.out.println(num8);
+        System.out.println(num9);
+        System.out.println(num10);
+    }
+}
