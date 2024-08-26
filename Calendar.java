75d564ea7b2de82912b0e6793a4f35b4cee00e34
keshav-maheshwari707 <keshav.maheshwari707@gmail.com>
2024-08-26 22:40:07 +0530

    calendar

---
+++ Calendar.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Calendar {
+    public static void main(String args[]) {
+        Scanner myCal = new Scanner(System.in);
+        System.out.println("enter number of the month :");
+        int number = myCal.nextInt();
+
+        switch(number) {
+            case 1 : System.out.println("Month is January");
+            break;
+            case 2 : System.out.println("Month is Febrary");
+            break;
+            case 3 : System.out.println("Month is March");
+            break;
+            case 4 : System.out.println("Month is April");
+            break;
+            case 5 : System.out.println("Month is May");
+            break;
+            case 6 : System.out.println("Month is June");
+            break;
+            case 7 : System.out.println("Month is July");
+            break;
+            case 8 : System.out.println("Month is August");
+            break;
+            case 9 : System.out.println("Month is September");
+            break;
+            case 10 : System.out.println("Month is October");
+            break;
+            case 11 : System.out.println("Month is November");
+            break;
+            case 12 : System.out.println("Month is December");
+            break;
+        }
+    }
+}
