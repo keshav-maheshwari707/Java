025e31657485259b23728aa08a0a8661ce417a08
keshav-maheshwari707 <keshav.maheshwari707@gmail.com>
2024-08-27 20:07:01 +0530

    for loop

---
+++ Loops.java
@@ -0,0 +1,13 @@
+public class Loops {
+    public static void main(String args[]) {
+        // FOR LOOP
+        
+        // for(int counter = 0; counter < 3; counter ++) {
+            // System.out.println("hello world!");
+        // }
+
+        for(int counter = 0; counter < 11; counter ++) {
+            System.out.println(counter);
+        }
+    }
+}
